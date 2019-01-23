1f7765e5fbf46b9de2b06205141a79fbe5fcf75c
João Gardenberg <joaogardenberg@gmail.com>
2019-01-23 21:13:23 -0200

    Now user can login with both email and username

--- app/models/concerns/users/fields/custom.rb
+++ app/models/concerns/users/fields/custom.rb
@@ -2,6 +2,8 @@ module Users::Fields::Custom
   extend ActiveSupport::Concern
 
   included do
+    attr_writer :login
+
     field :type,       type: Integer, default: 0
     field :type_of,    type: Array
     field :name,       type: String
--- app/models/concerns/users/methods.rb
+++ app/models/concerns/users/methods.rb
@@ -9,11 +9,28 @@ module Users::Methods
     def type_name
       case type
       when 0
-        I18n.t('users.types.secretary')
-      when 1
         I18n.t('users.types.doctor')
+      when 1
+        I18n.t('users.types.secretary')
       else
         I18n.t('users.types.undefined')
+      end
+    end
+
+    def password_required?
+      new_record? || password.present? || password_confirmation.present?
+    end
+
+    def login
+      @login || self.username || self.email
+    end
+
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        self.any_of({ :username =>  /^#{Regexp.escape(login)}$/i }, { :email =>  /^#{Regexp.escape(login)}$/i }).first
+      else
+        super
       end
     end
   end
