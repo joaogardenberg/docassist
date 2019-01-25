6130c4aa71f89aace8931d1e5bef5e467912d564
João Gardenberg <joaogardenberg@gmail.com>
2019-01-25 03:42:31 -0200

    Modularized functions

--- app/javascript/components/NewUser.jsx
+++ app/javascript/components/NewUser.jsx
@@ -1,7 +1,6 @@
 import React         from 'react';
 import { reduxForm } from 'redux-form';
 import Form          from './FormUser';
-import * as Regex    from '../constants/Regex';
 import * as User     from '../constants/User';
 
 let NewUser = props => {
@@ -23,120 +22,16 @@ let NewUser = props => {
   );
 }
 
-function validateType(type) {
-  if (!User.TYPE_VALUES.includes(type)) {
-    return 'Opção inválida. Recarregue a página';
-  }
-
-  return null;
-}
-
-function validateTypeOf(type, typeOf) {
-  if (type === User.TYPE_VALUES[User.TYPE_VALUES.length - 1] && (!typeOf || typeOf.length < 1)) {
-    return 'Campo obrigatório';
-  }
-
-  return null;
-}
-
-function validateName(name) {
-  if (!name) {
-    return 'Campo obrigatório';
-  }
-
-  if (name.length > 100) {
-    return 'Máximo 100 caracteres';
-  }
-
-  if (!name.toLowerCase().match(Regex.LowercaseName)) {
-    return 'Não pode conter caracteres especiais';
-  }
-
-  return null;
-}
-
-function validateUsername(username) {
-  if (!username) {
-    return 'Campo obrigatório';
-  }
-
-  if (username.length > 50) {
-    return 'Máximo 50 caracteres';
-  }
-
-  if (!username.toLowerCase().match(Regex.Username)) {
-    return 'Só pode conter letras e números'
-  }
-
-  return null;
-}
-
-function validateEmail(email) {
-  if (!email) {
-    return 'Campo obrigatório';
-  }
-
-  if (email.length > 50) {
-    return 'Máximo 50 caracteres';
-  }
-
-  if (!email.toLowerCase().match(Regex.Email)) {
-    return 'E-mail inválido';
-  }
-
-  return null;
-}
-
-function validateEmailConfirmation(email, emailConfirmation) {
-  if (!emailConfirmation) {
-    return 'Campo obrigatório';
-  }
-
-  if (email !== emailConfirmation) {
-    return 'Os e-mails não conferem';
-  }
-
-  return null;
-}
-
-function validatePassword(password) {
-  if (!password) {
-    return 'Campo obrigatório';
-  }
-
-  if (password.length > 50) {
-    return 'Máximo 50 caracteres';
-  }
-
-  if (password.length < 6) {
-    return 'Mínimo 6 caracteres';
-  }
-
-  return null;
-}
-
-function validatePasswordConfirmation(password, passwordConfirmation) {
-  if (!passwordConfirmation) {
-    return 'Campo obrigatório';
-  }
-
-  if (password !== passwordConfirmation) {
-    return 'As senhas não conferem';
-  }
-
-  return null;
-}
-
 function validate(values) {
   const errors = {
-    type:                  validateType(values['type']),
-    type_of_alias:         validateTypeOf(values['type'], values['type_of_alias']),
-    name:                  validateName(values['name']),
-    username:              validateUsername(values['username']),
-    email:                 validateEmail(values['email']),
-    email_confirmation:    validateEmailConfirmation(values['email'], values['email_confirmation']),
-    password:              validatePassword(values['password']),
-    password_confirmation: validatePasswordConfirmation(values['password'], values['password_confirmation'])
+    type:                  User.validateType(values['type']),
+    type_of_alias:         User.validateTypeOf(values['type'], values['type_of_alias']),
+    name:                  User.validateName(values['name']),
+    username:              User.validateUsername(values['username']),
+    email:                 User.validateEmail(values['email']),
+    email_confirmation:    User.validateEmailConfirmation(values['email'], values['email_confirmation']),
+    password:              User.validatePasswordNew(values['password']),
+    password_confirmation: User.validatePasswordConfirmationNew(values['password'], values['password_confirmation'])
   };
 
   const ret = Object.keys(errors).reduce((final, key) => {
--- app/javascript/constants/User.js
+++ app/javascript/constants/User.js
@@ -1,3 +1,5 @@
+import * as Regex from './Regex';
+
 export const TYPES = [
   'Médico(a)',
   'Secretário(a)'
@@ -13,6 +15,134 @@ export function getTypeName(type) {
 
   if (index >= 0) {
     return TYPES[index];
+  }
+
+  return null;
+}
+
+export function validateType(type) {
+  if (!TYPE_VALUES.includes(type)) {
+    return 'Opção inválida. Recarregue a página';
+  }
+
+  return null;
+}
+
+export function validateTypeOf(type, typeOf) {
+  if (type === TYPE_VALUES[TYPE_VALUES.length - 1] && (!typeOf || typeOf.length < 1)) {
+    return 'Campo obrigatório';
+  }
+
+  return null;
+}
+
+export function validateName(name) {
+  if (!name) {
+    return 'Campo obrigatório';
+  }
+
+  if (name.length > 100) {
+    return 'Máximo 100 caracteres';
+  }
+
+  if (!name.toLowerCase().match(Regex.LowercaseName)) {
+    return 'Não pode conter caracteres especiais';
+  }
+
+  return null;
+}
+
+export function validateUsername(username) {
+  if (!username) {
+    return 'Campo obrigatório';
+  }
+
+  if (username.length > 50) {
+    return 'Máximo 50 caracteres';
+  }
+
+  if (!username.toLowerCase().match(Regex.Username)) {
+    return 'Só pode conter letras e números'
+  }
+
+  return null;
+}
+
+export function validateEmail(email) {
+  if (!email) {
+    return 'Campo obrigatório';
+  }
+
+  if (email.length > 50) {
+    return 'Máximo 50 caracteres';
+  }
+
+  if (!email.toLowerCase().match(Regex.Email)) {
+    return 'E-mail inválido';
+  }
+
+  return null;
+}
+
+export function validateEmailConfirmation(email, emailConfirmation) {
+  if (!emailConfirmation) {
+    return 'Campo obrigatório';
+  }
+
+  if (email !== emailConfirmation) {
+    return 'Os e-mails não conferem';
+  }
+
+  return null;
+}
+
+export function validatePasswordNew(password) {
+  if (!password) {
+    return 'Campo obrigatório';
+  }
+
+  if (password.length > 50) {
+    return 'Máximo 50 caracteres';
+  }
+
+  if (password.length < 6) {
+    return 'Mínimo 6 caracteres';
+  }
+
+  return null;
+}
+
+export function validatePasswordEdit(password) {
+  if (password && password.length > 50) {
+    return 'Máximo 50 caracteres';
+  }
+
+  if (password && password.length < 6) {
+    return 'Mínimo 6 caracteres';
+  }
+
+  return null;
+}
+
+export function validatePasswordConfirmationNew(password, passwordConfirmation) {
+  if (!passwordConfirmation) {
+    return 'Campo obrigatório';
+  }
+
+  if (password !== passwordConfirmation) {
+    return 'As senhas não conferem';
+  }
+
+  return null;
+}
+
+export function validatePasswordConfirmationEdit(password, passwordConfirmation) {
+  if (password && !passwordConfirmation) {
+    return 'Campo obrigatório';
+  }
+
+  if (password && password !== passwordConfirmation) {
+    return 'As senhas não conferem';
   }
 
   return null;
