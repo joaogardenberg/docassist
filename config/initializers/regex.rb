14a7c5e1d51400bd1b782002378926d2b217030a
João Gardenberg <joaogardenberg@gmail.com>
2019-01-29 11:32:58 -0200

    Fixed url regex

--- config/initializers/regex.rb
+++ config/initializers/regex.rb
@@ -4,4 +4,4 @@ EMAIL_REGEX    = /\A[^@]+@[^@]+\.[^@]+\z/.freeze
 CPF_REGEX      = /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/.freeze
 PHONE_REGEX    = /\A\(\d{2}\)\s\d{4,5}-\d{4}\z/.freeze
 CEP_REGEX      = /\A\d{5}-\d{3}\z/.freeze
-URL_REGEX      = /\A(http|https):\/\/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+(\/.*)?(\?.*)?\z/.freeze
+URL_REGEX      = /\A(http|https):\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(\/.*)?(\?.*)?\z/.freeze
