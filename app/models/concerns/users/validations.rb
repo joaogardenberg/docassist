76f8fa932aab064adde8e5e73a1cf211218e2c06
João Gardenberg <joaogardenberg@gmail.com>
2019-01-29 11:34:09 -0200

    Better for reading

--- app/models/concerns/users/validations.rb
+++ app/models/concerns/users/validations.rb
@@ -2,15 +2,45 @@ module Users::Validations
   extend ActiveSupport::Concern
 
   included do
-    validates :user, presence: true, unless: :main?
-    validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }, length: { maximum: 50 }
-    validates :type, presence: true, inclusion: { in: self::TYPES }
-    validates :type_of, presence: true, unless: :doctor?
-    # Falta validar se cada valor do type_of é acessível ao current_user
-    validates :name, presence: true, format: { with: NAME_REGEX }, length: { maximum: 100 }
-    validates :username, presence: true, uniqueness: true, format: { with: USERNAME_REGEX }, length: { maximum: 50 }
-    validates :picture, format: { with: URL_REGEX }, allow_blank: true
-    validates :background, format: { with: URL_REGEX }, allow_blank: true
+    validates :user,
+              presence: true
+
+    validates :email,
+              presence: true,
+              uniqueness: true,
+              format: { with: EMAIL_REGEX },
+              length: { maximum: 50 }
+
+    validates :email_confirmation,
+              presence: true,
+              inclusion: { in: lambda { |user| [user.email] } }
+
+    validates :type,
+              presence: true,
+              inclusion: { in: self::TYPES }
+
+    validates :type_of,
+              presence: true,
+              unless: :doctor?
+
+    validates :name,
+              presence: true,
+              format: { with: NAME_REGEX },
+              length: { maximum: 100 }
+
+    validates :username,
+              presence: true,
+              uniqueness: true,
+              format: { with: USERNAME_REGEX },
+              length: { maximum: 50 }
+
+    validates :picture,
+              format: { with: URL_REGEX },
+              allow_blank: true
+
+    validates :background,
+              format: { with: URL_REGEX },
+              allow_blank: true
 
     validate :validate_type_of
 
