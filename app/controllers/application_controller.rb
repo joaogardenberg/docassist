3f6c3cacccb3d64a3bb216a8c3a34c3a06606812
João Gardenberg <joaogardenberg@gmail.com>
2019-02-02 02:16:41 -0200

    Fixed logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def index
     redirect_to :system
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    :new_user_session
   end
 end
